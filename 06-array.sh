e2fd422dbb72185d627fb9412d4cd6e0081a291f
Sundari <ravicvsr53@gmail.com>
2025-02-21 20:33:20 +0530

    hi

--- 06-array.sh
+++ 06-array.sh
@@ -2,10 +2,10 @@
 
 MOVIES=("RRR" "DJ Tillu" "Bahubali")
 
-echo "First movie is: ${MOVIES[0]}"
+echo "First movie is: $MOVIES[0]"
 
-echo "First movie is: ${MOVIES[1]}"
+echo "First movie is: $MOVIES[1]"
 
-echo "First movie is: ${MOVIES[2]}"
+echo "First movie is: $MOVIES[2]"
 
-echo "First movie is: ${MOVIES[@]}"
+echo "First movie is: $MOVIES[@]"
